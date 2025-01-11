cb4a1612eb64398e3ead042b960a54f398fe1cfd
danikua <daniilbokach@gmail.com>
2025-01-11 18:36:12 +0200

    imports fixing

--- fake-store/src/components/CartItem.tsx
+++ fake-store/src/components/CartItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {
-  ListItem,
-  ListItemAvatar,
   Avatar,
   IconButton,
   TextField,
--- fake-store/src/main.tsx
+++ fake-store/src/main.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import { createTheme } from '@mui/material';
 import { App } from './App';
 import './index.css';
 
